0cbbfb1bb9f263969dd95940ddec7ad226846697
Brayzen <brian.ray.biz@gmail.com>
2015-10-13 14:57:57 -0700

    add controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api, path: '/', contraints: { subdomain: 'api' } do
+  namespace :api, path: '/', contraints: { subdomain: 'api' }, defaults: {format: :json} do
     resources :stones
   end
 end
