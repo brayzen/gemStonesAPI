8032eae95d8eb14c26439e0479f64a0d165cd63f
Brayzen <brian.ray.biz@gmail.com>
2015-10-14 15:55:13 -0700

    import js files

--- app/controllers/api/stones_controller.rb
+++ app/controllers/api/stones_controller.rb
@@ -5,19 +5,30 @@ class API::StonesController < ApplicationController
     render json: @stones.as_json(include: [:reviews, :images]) , status: :ok
   end
 
+  def create
+    stone = Stone.new(stone_params)
+
+    if stone.save
+      render json: stone, status: :created
+    else
+      render json: stone.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     @stone = Stone.find(params[:id])
     render json: @stone.as_json(include: [:reviews, :images]) , status: :ok
   end
 
-  def create
-  end
+  # def show
+  #   @stone = Stone.find(params[:id])
+  #   render json: { stone: @stone, desciption: @stone.description, name: @stone.name }
+  # end
 
-  def update
-  end
-
-  def destroy
-  end
+  private
+    def stone_params
+      params.require(:gemstone).permit(:name, :price)
+    end
 
 
 end
